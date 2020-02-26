583f70f710079b8c14ae9efb772f8ae6c42c282c
Josh Allen <joshmrallen@gmail.com>
2020-02-26 16:10:31 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -31,7 +31,11 @@ def directors_totals(nds)
   director_index = 0
   while director_index < nds.count do
     
-    result.push({"#{nds[director_index][:name]}" => gross_for_director(nds[director_index])})
+    result.push(
+      {
+      "#{nds[director_index][:name]}" => gross_for_director(nds[director_index])
+      }
+    )
     
     director_index += 1
   end  
